8f753e3c837194f3637b557166ae9904001c34af
Christian Nikkanen <Christian.Nikkanen@viestimedia.fi>
2023-09-14 14:24:08 +0300

    Handle openInNewTab

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -12,13 +12,21 @@ export const AnchorComponent = (props: AnchorProps) => {
 export const RemixLink =
   // What props need to be renamed/remapped between Next and Remix links
   (LinkComponent: RemixLinkType) => (props: NextLinkComponentProps) => {
-    const { href, prefetch, children, ...rest } = props;
+    const { href, prefetch, children, openInNewTab, ...rest } = props;
     // type PrefetchBehavior = 'intent' | 'render' | 'none' | 'viewport';
     // NOTE: we don't support PrefetchBehavior in Remix yet, only intent
     // Next only has prefetch as a boolean, so we map it to intent or none
     const remixPrefetch = prefetch ? 'intent' : 'none';
+    const target = openInNewTab ? '_blank' : undefined;
+    const rel = openInNewTab ? 'noopener noreferrer' : undefined;
     return (
-      <LinkComponent to={href} prefetch={remixPrefetch} {...rest}>
+      <LinkComponent
+        to={href}
+        prefetch={remixPrefetch}
+        target={target}
+        rel={rel}
+        {...rest}
+      >
         {children}
       </LinkComponent>
     );
