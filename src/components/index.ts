0f4346d0af6cd2562882f73f31e77bab481af456
Christian Nikkanen <Christian.Nikkanen@viestimedia.fi>
2024-01-30 17:39:24 +0200

    Turns out I edited a gitless copy when I fixed the export

--- src/components/index.ts
+++ src/components/index.ts
@@ -18,6 +18,7 @@ export * from './Tabs/Tabs';
 export * from './Tag/Tag';
 export * from './Teaser/Teaser';
 export * from './Footer/Footer';
+export * from './CookieSettingsLink/CookieSettingsLink';
 
 // This doesn't work with the * syntax for some reason
 export { initializeComponentLibrary } from 'utils/init';
