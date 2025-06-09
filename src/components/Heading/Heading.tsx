4008b2733c08151bb85978023505135216d92403
hennnio <henna@niola.fi>
2025-06-09 15:21:36 +0300

    Add new heading style used in player

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -16,6 +16,7 @@ export enum HeadingStyle {
   TeaserXS = 'teaserXs',
   ArticleHeading = 'articleHeading',
   ArticleSubheading = 'articleSubheading',
+  PlayerHeading = 'playerHeading',
 }
 
 export type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
