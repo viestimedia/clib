73d04967bd33d4bea7210f3c3e4de298cc08cf51
Henna <37694115+hennnio@users.noreply.github.com>
2026-03-23 15:28:30 +0200

    Fix deprecation warnings by refactoring SCSS imports to use new syntax (#232)

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,11 @@ export default defineConfig({
   },
 
   css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
     modules: {
       localsConvention: 'camelCaseOnly',
 
