95698292e42332f2fd88689e18f2ae9b6a8e5653
Johannes Raunio <johannes.raunio@viestimedia.fi>
2026-04-09 12:07:47 +0300

    Render teaser in correct size when image is missing

--- src/App.tsx
+++ src/App.tsx
@@ -405,8 +405,8 @@ function TeaserRoute() {
         <Teaser
           heading="Uuden sukupolven traktorit mullistavat maatilan arjen – katso vertailu"
           category="Traktorit"
-          linkUrl="/art4"
-          id="art3-no-image"
+          linkUrl="/art8"
+          id="art8"
           className="koneviesti"
           image={
             <img src="https://viestimedia.imgix.net/kSkT6-ee83GiM08noowjgzNhVJM.jpg?h=1312&fp-x=0.5&fp-y=0.5&crop=focalPoint&fit=crop&fp-z=1&ar=1%3A1" />
@@ -420,8 +420,8 @@ function TeaserRoute() {
             <Teaser
               heading="Talvihuolto kuntoon – näin pidät koneet valmiina kevääseen"
               category="Huolto ja tekniikka"
-              linkUrl="/cat1"
-              id="cat1"
+              linkUrl="/art9"
+              id="art9"
               className="koneviesti"
               teaserType={TeaserType.CategoryLatest}
               image={
@@ -434,8 +434,8 @@ function TeaserRoute() {
             <Teaser
               heading="Ennätyssato vaatii täsmäviljelyä – GPS ohjaa jo puimuria"
               category="Maatalous"
-              linkUrl="/cat2"
-              id="cat2"
+              linkUrl="/art10"
+              id="art10"
               className="koneviesti"
               teaserType={TeaserType.CategoryLatest}
               image={
@@ -445,6 +445,20 @@ function TeaserRoute() {
             />
           </div>
         </div>
+        {/* Test missing image */}
+        <Teaser
+          heading="Uuden sukupolven traktorit mullistavat maatilan arjen – katso vertailu"
+          category="Traktorit"
+          linkUrl="/art11-no-image"
+          id="art11-no-image"
+          className="koneviesti"
+          image={
+            <img src="" />
+          }
+          teaserType={TeaserType.CategoryLatest}
+          date="25.3.2026"
+          tag={<Tag text="Tilaajalle" type={TagType.PremiumLight} />}
+        />
       </div>
       <div className="bg-gray-200 p-3 border flex flex-col gap-4 max-w-2xl">
         <Carousel
